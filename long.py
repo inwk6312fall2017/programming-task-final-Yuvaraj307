82799a3f0da24909b40ca8d4f7b7842a7502bb76
Parik Dhiman <parik.dhiman@dal.ca>
2017-10-20 15:09:34 -0300

    print days

--- long.py
+++ long.py
@@ -3,14 +3,33 @@ from collections import Counter
 longest = 0
 
 with open('Book1.txt', 'r') as handle:
-	for word in handle:
-		word.split()
-		word.strip()
-		print(word)
-		if len(word) > longest:
-			longest = len(word)
-			longest_word = word
-			print(longest_word)
+	for word1 in handle:
+		word1.split()
+		word1.strip()
+
+with open('Book2.txt', 'r') as handle:
+	for word2 in handle:
+		word2.split()
+		word2.strip()
+
+with open('Book3.txt', 'r') as handle:
+	for word3 in handle:
+		word3.split()
+		word3.strip()
+
+		if len(word1) > len(word2):
+			longest = len(word1)
+			longest_word = word1
+			
+		elif len(word2) > len(word3):
+			longest = len(word2)
+			longest_word = word2
+			
+		elif len(word1) > len(word3):
+			longest = len(word3)
+			longest_word = word3
+		
+	print(longest_word)
  
            
 
